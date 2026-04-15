dff9e07e6932928b9f2756ec7415008c00920f19
Darnikus <fake@email.com>
2026-04-15 22:26:03 +0300

    refactor(client) : replace threading with asyncio for whole client side

--- client/bluetooth_driver.py
+++ client/bluetooth_driver.py
@@ -1,7 +1,7 @@
 import socket
 import logging
-import time
 from secrets import server_address, port
+import asyncio
 
 
 logger = logging.getLogger(__name__)
@@ -10,28 +10,37 @@ class BluetoothDriver:
 
     def __init__(self) -> None:
         self._sock: socket.socket | None = None
+        self.reader = None
+        self.writer = None
 
-    def connect(self) -> bool:
+    async def connect(self) -> bool:
         self.disconnect()
-        time.sleep(5)
 
         logger.info(f"--- Attempting connection to {server_address} ---")
-        try:
-            self._sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-            self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            self._sock.settimeout(10) # Don't hang forever on connect
-            self._sock.connect((server_address, port))
-            self._sock.settimeout(None) # Go back to blocking mode
+        self._sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+        self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self._sock.setblocking(False)
 
+        loop = asyncio.get_running_loop()
+        try:
+            await asyncio.wait_for(
+                loop.sock_connect(self._sock, (server_address, port)),
+                timeout=10.0
+            ) 
+            
+            self.reader, self.writer = await asyncio.open_connection(sock=self._sock)
             logger.info("Connected to ESP32 (SPP)!")
             return True
-        except socket.error:
+        except Exception:
             logger.exception('Failed to connect.')
             self.disconnect()
             return False
 
     def disconnect(self) -> None:
         """Save socket disconnect and close"""
+        if self.writer:
+            self.writer.close()
+        
         if self._sock:
             try:
                 self._sock.shutdown(socket.SHUT_RDWR)
@@ -40,23 +49,27 @@ class BluetoothDriver:
             
             self._sock.close()
             self._sock = None
+            self.reader = None
+            self.writer = None
             logger.info('Connection is closed.')
 
     
-    def send_data(self, data: str) -> bool:
-        if not self._sock:
+    async def send_data(self, data: str) -> bool:
+        if not self.writer or self.writer.is_closing():
             logger.error("There is not connection.")
-            if not self.connect():
+            if not await self.connect():
                 return False
         try:
-            self._sock.send(data.encode('utf-8'))
+            self.writer.write(data.encode('utf-8'))
+            await self.writer.drain()
             logger.info(f"Sent: {data}")
             return True
-        except socket.error:
+        except Exception:
             logger.exception(f'Failed to send: {data}. Performing disconnection.')
             self.disconnect()
             return False
 
+    # Don't know if i need it, at least it's needed to be refactored for asyncio
     def _is_connected(self) -> bool:
         try:
             self._sock.getpeername()
--- client/main.py
+++ client/main.py
@@ -2,7 +2,7 @@ import time
 import logging
 from bluetooth_driver import BluetoothDriver
 
-import threading
+import asyncio
 import random
 
 
@@ -14,10 +14,13 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 # Get a logger instance
 logger = logging.getLogger(__name__)
 
-def input_thread():
+async def input_thread():
     global sending_flag
+    loop = asyncio.get_running_loop()
+
     while True:
-        cmd = input("Type '-' to pause, '+' to resume: \n").strip().lower()
+        cmd = await loop.run_in_executor(None, lambda: input("Type '-' to pause, '+' to resume: \n").strip().lower())
+        
         if cmd == '-':
             sending_flag = False
             print("--- SENDING PAUSED ---")
@@ -26,35 +29,51 @@ def input_thread():
             print("--- SENDING RESUMED ---")
 
 
-def connect_and_send():
-    sock: BluetoothDriver = BluetoothDriver()
-
+async def connect_and_send(sock: BluetoothDriver) -> None:
     last_heartbeat = time.time()
     while True:
         try:
-            while True:
-                message = "a"
-                if sending_flag:
-                    if not sock.send_data(message):
-                        logger.error("Could not send data. Waiting for next cycle...")
-                        time.sleep(3)
-                        continue
-                    
-                    # If you don't receive data, the script won't know the 
-                    # socket is dead until the next .send() call fails.
-                    time.sleep(4 + random.randint(0, 200) / 1000)
+            message = "a"
+            if sending_flag:
+                if not await sock.send_data(message):
+                    logger.error("Could not send data. Waiting for next cycle...")
+                    await asyncio.sleep(3)
+                    continue
                 
-                elif time.time() - last_heartbeat > 5:
-                    sock.send_data('\n')
-                    last_heartbeat = time.time()
+                # If you don't receive data, the script won't know the 
+                # socket is dead until the next .send() call fails.
+                await asyncio.sleep(4 + random.randint(0, 200) / 1000)
+            
+            elif time.time() - last_heartbeat > 5:
+                await sock.send_data('\n')
+                last_heartbeat = time.time()
+
+                await asyncio.sleep(0.1)
 
         except KeyboardInterrupt:
             logger.error("\nUser stopped script.")
             sock.disconnect()
             break
 
-if __name__ == "__main__":
-    t = threading.Thread(target=input_thread, daemon=True)
-    t.start()
+async def main():
+    sock: BluetoothDriver = BluetoothDriver()
 
-    connect_and_send()
+    if not await sock.connect():
+        logger.exception("Initial connection failed. Driver will try to reconnect later.")
+
+    try:
+        await asyncio.gather(
+            input_thread(),
+            connect_and_send(sock)
+        )
+    except asyncio.CancelledError:
+        pass
+    finally:
+        sock.disconnect()
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except Exception as e:
+        pass
