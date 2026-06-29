2b0c963798e7e0f3cfd59e76a3adc7061da8f47a
Darnikus <fake@email.com>
2026-06-29 18:34:28 +0300

    feature(client): add event handlers for SavePresetScreen

--- client/ui/save_preset_screen.py
+++ client/ui/save_preset_screen.py
@@ -33,5 +33,48 @@ class SavePresetScreen(ModalScreen):
 
             # Row 3: Footer
             with Container(id="bottom-container"):
-                yield Button("Save", variant="success", id="add-button")
+                yield Button("Save", variant="success", id="save-button")
                 yield Button("Cancel", variant="primary", id="cancel-button")
+
+    def on_input_changed(self, event: Input.Changed) -> None:
+        """Updates and toggles the error labels as the user types."""
+        if not event.input or not event.input.id:
+            return
+
+        # I don't want to have validation for description yet
+        if event.input.id[:-6] == "description":
+            return
+
+        error_label_id = f"#{event.input.id[:-6]}-error"
+        error_label = self.query_one(error_label_id, Label)
+
+        if event.validation_result and not event.validation_result.is_valid:
+            error_label.update(event.validation_result.failure_descriptions[0])
+            error_label.remove_class("hidden")
+        else:
+            error_label.add_class("hidden")
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "cancel-button":
+            self.app.pop_screen()
+
+        elif event.button.id == "save-button":
+            filename_input = self.query_one("#file-name-input", Input)
+            description_input = self.query_one("#description-input", Input)
+
+            filename_input.validate(filename_input.value)
+            # description_input.validate(description_input.value)
+
+            if self.query("Input.-invalid"):
+                self.notify("Please fill out all fields correctly.", severity="error")
+            # elif self._is_duplicate(filename_input.value):
+            #     self.notify(
+            #         f"'{filename_input.value}' already exists!", severity="warning"
+            #     )
+            else:
+                self.dismiss(
+                    (
+                        filename_input.value,
+                        description_input.value,
+                    )
+                )
