2fa7779f5588cd592dbd328156e47a3a4afb88a5
Darnikus <fake@email.com>
2026-07-15 20:45:39 +0300

    refactor(client): move providers to their directory

--- client/ui/app.py
+++ client/ui/app.py
@@ -9,9 +9,9 @@ from textual.widgets import DataTable, Footer, Header, Log
 from manager.key_manager import KeyManager
 from ui.add_key_screen import AddKeyScreen
 from ui.edit_key_screen import EditKeyScreen
-from ui.load_preset_provider import LoadPresetProvider
 from ui.load_preset_screen import LoadPresetScreen
-from ui.save_preset_provider import SavePresetProvider
+from ui.providers.load_preset_provider import LoadPresetProvider
+from ui.providers.save_preset_provider import SavePresetProvider
 from ui.save_preset_screen import SavePresetScreen
 from utility.log_config import link_textual_ui
 
