c64295842197bcd7d71fa425bb1628d6791ed484
Darnikus <fake@email.com>
2026-08-09 00:12:06 +0300

    feature(client): remove commands from the command palette in ListenerScreen
    I will continue using _toggle_command_palette until I implement MainScreen and assign COMMANDS there.

--- client/ui/screens/listener_screen.py
+++ client/ui/screens/listener_screen.py
@@ -13,9 +13,23 @@ class ListenerScreen(Screen):
         yield RichLog(id="api-log", highlight=True, markup=True)
 
     def on_mount(self) -> None:
+        self._toggle_command_palette(True)
+
         log = self.query_one(RichLog)
         log.write("[green]Listener screen is mounted.[/green]")
+
+    def _on_unmount(self) -> None:
+        self._toggle_command_palette(False)
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         if event.button.id == "back-button":
             self.app.pop_screen()
+
+    def _toggle_command_palette(self, state: bool) -> None:
+        """Removes all commands from the command palette if state is True.\n
+        Because custom providers are assigned in App, this method exists."""
+        if state:
+            self._old_app_commands = self.app.COMMANDS
+            self.app.COMMANDS = set()
+        else:
+            self.app.COMMANDS = self._old_app_commands
